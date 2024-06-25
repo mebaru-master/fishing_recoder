31bb9b958a766f6e7d6309b92946849dd011cbf6
mebaru-master <154340605+mebaru-master@users.noreply.github.com>
2024-06-25 05:00:04 +0000

    Sample comminucation python commit

---
+++ Communication/sample_comm.py
@@ -0,0 +1,19 @@
+# 富山県の週間天気予報を取得して表示するプログラム
+# 2020/07/01
+# 2020/07/02
+
+import requests
+import json
+import pprint
+
+# 週間天気予報を取得するURL
+url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=160010'
+
+# 週間天気予報を取得する
+response = requests.get(url)
+data = response.json()
+pprint.pprint(data)
+# 週間天気予報を表示する
+for weather in data['forecasts']:
+    print(weather['dateLabel'] + ' : ' + weather['telop'])
+
